69b4d429b955d6791ee11a550707a7b8e60171c7
HYUNJUN <guswns1659@gmail.com>
2020-07-10 12:29:20 +0900

    [BE] feat : feed에 공간데이터 삽입 성공

--- BE/src/main/java/me/titatic/hackatonpractice/domain/Feed.java
+++ BE/src/main/java/me/titatic/hackatonpractice/domain/Feed.java
@@ -1,6 +1,9 @@
 package me.titatic.hackatonpractice.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
+
+import org.locationtech.jts.geom.Point;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -16,4 +19,7 @@ import lombok.NoArgsConstructor;
 public class Feed {
 
     private Integer likeCount;
+
+    @Column
+    private Point point;
 }
--- BE/src/test/java/me/titatic/hackatonpractice/domain/AccountRepositoryTest.java
+++ BE/src/test/java/me/titatic/hackatonpractice/domain/AccountRepositoryTest.java
@@ -5,6 +5,9 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.io.ParseException;
+import org.locationtech.jts.io.WKTReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,21 +19,28 @@ public class AccountRepositoryTest {
     @Autowired
     private AccountRepository accountRepository;
 
-    @DisplayName("사용자와 피드를 저장하는 테스트")
+    @DisplayName("사용자와 위치정보를 가진 피드를 저장하는 테스트")
     @CsvSource({"accountName, 2"})
     @ParameterizedTest
-    void 유저와_피드를_저장한다(String accountName, String likeCount) {
+    void 유저와_피드를_저장한다(String accountName, Integer likeCount) throws ParseException {
         Account account = Account.builder()
             .name(accountName)
             .build();
 
+        Double latitude = 32.123;
+        Double longitude = 127.123;
+        String pointWKT = String.format("POINT(%s %s)", longitude, latitude);
+        Point point = (Point) new WKTReader().read(pointWKT);
+
         Feed feed = Feed.builder()
-            .likeCount(Integer.parseInt(likeCount))
+            .likeCount(likeCount)
+            .point(point)
             .build();
 
         account.addFeed(feed);
         accountRepository.save(account);
 
-        assertThat(account.getFeeds().iterator().next().getLikeCount()).isEqualTo(Integer.parseInt(likeCount));
+        assertThat(account.getFeeds().iterator().next().getLikeCount()).isEqualTo(likeCount);
+        assertThat(account.getFeeds().iterator().next().getPoint().getX()).isEqualTo(longitude);
     }
 }
